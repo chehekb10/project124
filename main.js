25b0abfc553a593d70db93e44bf0740ee7f0d60d
chehekb10 <72211025+chehekb10@users.noreply.github.com>
2022-03-31 15:34:05 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload()
+{
+
+}
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(500,500);
+    
+    canvas = createCanvas(350, 350);
+    canvas.position(560,150);
+
+    poseNet= ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+}
+function modelLoaded()
+{
+    console.log('PoseNet is initialzed!');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0);
+    {
+        console.log(results);
+    }
+}
+function draw()
+{
+    
+}
